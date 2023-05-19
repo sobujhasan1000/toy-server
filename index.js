d21332eab9d124f7f235865f1fcef74796e21c9d
Mohidul islam <sobujhasan1000@gamil.com>
2023-05-19 23:32:53 +0600

    post data complite

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zunrmyl.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+// console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,12 +25,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const animalsCollection=client.db('animalsDB').collection('animals');
+
+    app.get('/postanimals', async(req,res)=>{
+      const cursor=animalsCollection.find();
+      const result=await cursor.toArray();
+      res.send(result)
+    })
+
+app.post('/postanimals', async(req, res)=>{
+  const newAnimals=req.body;
+  console.log(newAnimals);
+  const result= await animalsCollection.insertOne(newAnimals);
+  res.send(result)
+})
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
